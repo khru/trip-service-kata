7aec7864457e431b5c7ae52162e084fc8c9c4994
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-04-23 21:48:13 +0200

    refactor: remove user session dependency and pass it as argument on the contract

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,35 +1,26 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
-import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		User loggedInUser = this.getLoggedUser();
-		if (null == loggedInUser) {
-			throw new UserNotLoggedInException();
-		}
+    public List<Trip> getTripsByUser(User user, User loggedInUser) throws UserNotLoggedInException {
+        if (null == loggedInUser) {
+            throw new UserNotLoggedInException();
+        }
 
-		if (!user.isFriendsWith(loggedInUser)) {
-			return Collections.emptyList();
-		}
+        if (!user.isFriendsWith(loggedInUser)) {
+            return Collections.emptyList();
+        }
 
-		return tripsBy(user);
-	}
+        return tripsBy(user);
+    }
 
-	List<Trip> tripsBy(User user) {
-		return TripDAO.findTripsByUser(user);
-	}
-
-	public User getLoggedUser() {
-		return UserSession.getInstance().getLoggedUser();
-	}
-
+    List<Trip> tripsBy(User user) {
+        return TripDAO.findTripsByUser(user);
+    }
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -16,7 +16,6 @@ public class TripServiceShould {
 
     public static final User GUEST = null;
     public static final User REGISTER_USER = new User();
-    public static final User ANOTHER_USER = new User();
     public static final Trip LONDON_TRIP = new Trip();
     public static final Trip BARCELONA_TRIP = new Trip();
     private User loggedInUser;
@@ -29,54 +28,43 @@ public class TripServiceShould {
         userBuilder = new UserBuilder();
         tripService = new TestableTripService();
     }
-	@Test
-    public void not_allowed_not_logged_in_users() {
-        loggedInUser = GUEST;
 
+    @Test
+    public void not_allowed_not_logged_in_users() {
         UserNotLoggedInException expectedException = assertThrows(UserNotLoggedInException.class, () -> {
-            tripService.getTripsByUser(loggedInUser);
+            tripService.getTripsByUser(loggedInUser, GUEST);
         });
     }
 
     @Test
     public void return_an_empty_trip_list_when_the_user_has_no_friends() {
-
         User otherStranger = userBuilder.build();
         User stranger = userBuilder.withFriends(otherStranger).build();
 
-        loggedInUser = userBuilder.build();
-
-        assertEquals(tripService.getTripsByUser(stranger), Collections.emptyList());
+        assertEquals(tripService.getTripsByUser(stranger, REGISTER_USER), Collections.emptyList());
     }
 
     @Test
     public void return_an_empty_trip_list_when_the_user_has_friends_but_not_trips() {
-
-        loggedInUser = REGISTER_USER;
-        User anotherUser = new UserBuilder()
-                .withFriends(loggedInUser)
+        User anotherUser = userBuilder
+                .withFriends(REGISTER_USER)
                 .build();
 
-        assertEquals(tripService.getTripsByUser(anotherUser), Collections.emptyList());
+        assertEquals(tripService.getTripsByUser(anotherUser, REGISTER_USER), Collections.emptyList());
     }
 
     @Test
     public void return_a_list_of_trips_when_the_user_has_friends_and_trips() {
-        loggedInUser = REGISTER_USER;
-        User friend = new UserBuilder()
-                .withFriends(loggedInUser)
+        User friend = userBuilder
+                .withFriends(REGISTER_USER)
                 .withTrips(LONDON_TRIP, BARCELONA_TRIP)
                 .build();
 
-        assertEquals(tripService.getTripsByUser(friend), List.of(LONDON_TRIP, BARCELONA_TRIP));
+        assertEquals(tripService.getTripsByUser(friend, REGISTER_USER), List.of(LONDON_TRIP, BARCELONA_TRIP));
     }
 
     class TestableTripService extends TripService {
 
-        @Override
-        public User getLoggedUser() {
-            return loggedInUser;
-        }
         @Override
         public List<Trip> tripsBy(User user) {
             return user.trips();
